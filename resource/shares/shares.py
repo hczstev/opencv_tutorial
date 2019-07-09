7c6f7868d30d7bd6a02f2f9df6dacf756c044984
hczstev <hczstev@foxmail.com>
2019-07-09 19:36:22 +0800

    Adding gradient.png

--- resource/shares/shares.py
+++ resource/shares/shares.py
@@ -2,9 +2,9 @@ from cv2 import imshow, waitKey, destroyAllWindows
 from os.path import abspath, join
 
 root_path = abspath("../")
-messi_image = join(root_path, "data", "messi5.jpg")
-print(messi_image)
 
+messi_image = join(root_path, "data", "messi5.jpg")
+gradient_image = join(root_path, "data", "gradient.png")
 
 def show_image(img):
     imshow('image', img)
