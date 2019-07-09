b664f37ac1f49e905804230963efdfd6b4afdeed
hczstev <hczstev@foxmail.com>
2019-07-09 15:05:55 +0800

    Adding abspath and messi_image

--- resource/shares/shares.py
+++ resource/shares/shares.py
@@ -1,4 +1,9 @@
 from cv2 import imshow, waitKey, destroyAllWindows
+from os.path import abspath, join
+
+root_path = abspath("../")
+messi_image = join(root_path, "data", "messi5.jpg")
+print(messi_image)
 
 
 def show_image(img):
